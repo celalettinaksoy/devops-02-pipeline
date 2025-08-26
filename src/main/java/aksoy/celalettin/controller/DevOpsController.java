2cbcf9a6d1685e5e4160d0939c8c7c40cf561dff
Celalettin Aksoy <celalettinaksoyy@gmail.com>
2025-08-26 22:36:23 +0300

    Devops Pipeline files added
    Devops Pipeline files added

---
+++ src/main/java/aksoy/celalettin/controller/DevOpsController.java
@@ -0,0 +1,48 @@
+package aksoy.celalettin.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+//    http://localhost:8080
+@RestController
+@RequestMapping
+public class DevOpsController {
+
+    // f(x) = 2x + 3
+  /*
+  public float   f(x) {
+      return  2*x + 3;
+    }
+ */
+
+    //    http://localhost:8080
+    @GetMapping
+    public String devopsHello() {
+        return "Version3 Aksoy Merhaba: " + LocalDateTime.now();
+    }
+
+
+    //    http://localhost:8080/info
+    @GetMapping("info")
+    public String info() {
+        return "Version3 DEVOPS Aksoy INFO: " + LocalDateTime.now();
+    }
+
+
+    //    http://localhost:8080/about
+    @GetMapping("about")
+    public String about() {
+        return "Version3 about: " + LocalDateTime.now();
+    }
+
+
+    //    http://localhost:8080/about
+    @GetMapping("entry")
+    public String entry() {
+        return "Version3 entry: " + LocalDateTime.now();
+    }
+
+}
